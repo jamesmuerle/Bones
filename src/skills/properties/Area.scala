35c6d506c40f9be719be5a8e6fd6ebe59b03e059
jamesmuerle <jamesmuerle@gmail.com>
2014-01-26 11:43:41 -0800

    Now the gitignore and Area should actually be committed.

--- src/skills/properties/Area.scala
+++ src/skills/properties/Area.scala
@@ -4,23 +4,26 @@ import gamestate.GameState
 import drawables.map.cell.Cell
 
 /**
+ * An Area is an area of effect of a skill.
+ * The required method is one that given a gamestate, an origin cell, and a target cell, returns a list of cells
+ * that are in the area of effect.
  * Created by James on 1/14/14.
  */
 trait Area {
   def getCells(gamestate: GameState, origin: Cell, target: Cell): List[Cell]
 }
 
-case class LineArea(val length: Int) extends Area {
+case class LineArea(length: Int) extends Area {
   def getCells(gamestate: GameState, origin: Cell, target: Cell): List[Cell] = {
     null // TODO
   }
 }
-case class ConeArea(val length: Int) extends Area {
+case class ConeArea(length: Int) extends Area {
   def getCells(gamestate: GameState, origin: Cell, target: Cell): List[Cell] = {
     null // TODO
   }
 }
-case class SquareArea(val dim: Int, val diag: Boolean) extends Area {
+case class SquareArea(dim: Int, diag: Boolean) extends Area {
   def getCells(gamestate: GameState, origin: Cell, target: Cell): List[Cell] = {
     null // TODO
   }
